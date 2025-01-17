3302d8e5e5903af8241693bba48ddd49d8d51690
IsaacKeen1 <IsaacKeen2004@gmail.com>
2025-01-16 16:08:23 -0800

    Initial Commit

---
+++ FlowState/FlowStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowStateApp.swift
+//  FlowState
+//
+//  Created by Isaac Keen on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct FlowStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlowStateTests/FlowStateTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlowStateTests.swift
+//  FlowStateTests
+//
+//  Created by Isaac Keen on 1/16/25.
+//
+
+import Testing
+@testable import FlowState
+
+struct FlowStateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
